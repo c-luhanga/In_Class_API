1f0e8faaff41b0b366814030762af4f4e0bbb071
c-luhanga <charles.luhanga@principia.edu>
2025-09-14 14:35:05 -0500

    Completed API call to JokesAPI

---
+++ CoindeskApiClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url = "https://v2.jokeapi.dev/joke/Any";
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("JokeAPI Response:");
+                Console.WriteLine(responseBody);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
